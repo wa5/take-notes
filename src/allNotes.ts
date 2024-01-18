de9fb3f98df786e89aaa80bf86f87bd697f32fb6
wa5 <waseemslyar@gmail.com>
2024-01-18 09:35:27 +0530

    l

--- src/allNotes.ts
+++ src/allNotes.ts
@@ -1,8 +1,17 @@
 let url='https://cdn.jsdelivr.net/gh/wa5/practise-delete/abc-test.pdf'
+let base=`https://cdn.jsdelivr.net/gh/`
+let urlBulder=`${base}`
+import {data} from './data'
+
 export const allNotes={
     all:()=>{
         return [url]
     },
+    student:(name:string)=>{
+        let info=data[0].students.find((val)=>{return val.studentName===name})
+        let exurlBulder=`${urlBulder}/${info?.gitHub}/${info?.repo}/${info?.fileName}`
+        return {fileurl:exurlBulder,studentInfo:info}
+    }
 
 
 }
---
+++ src/data.ts
@@ -0,0 +1,16 @@
+export const data=[{
+    students: [
+        {
+            studentName:'jayanth',
+            gitHub:"BR-Jayanth",
+            repo:"take-notes-lib",
+            fileName:'Jayanthbr.pdf',
+            language:'js',
+            topic:'for loop'
+    
+  
+}]
+}
+  
+]
+   
