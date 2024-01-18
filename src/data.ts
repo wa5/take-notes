d8c5e732173977bbdbff77837e64d3f47d4e6910
wa5 <waseemslyar@gmail.com>
2024-01-18 11:43:53 +0530

    l

--- src/data.ts
+++ src/data.ts
@@ -7,8 +7,6 @@ export const data=[{
             fileName:'Jayanthbr.pdf',
             language:'js',
             topic:'for loop'
-    
-  
 },{
     studentName:"Safrin Sara",
             gitHub:"codehero11",
@@ -16,6 +14,21 @@ export const data=[{
             fileName:'JAVA.pdf',
             language:'java',
             topic:'int'
+},
+{
+    studentName:'Manoj M',
+            gitHub:"skylinedude",
+            repo:"My-notes",
+            fileName:'Java Strings.pdf',
+            language:'java',
+            topic:'Strings'
+},{
+    studentName:'Chandan N',
+            gitHub:"skylinedude",
+            repo:"My-notes",
+            fileName:'Abstract in java.pdf',
+            language:'java',
+            topic:'Abstract Keyword'
 }
 ]
 }
